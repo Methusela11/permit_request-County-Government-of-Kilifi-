e0564a55b47c8d06ad37bd8838a47f9d4e9c1d8e
Methusela11 <soenkilifi.com>
2026-02-25 02:02:47 +0300

    sstatic

--- permit_request/settings.py
+++ permit_request/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # Serve static files in production
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
